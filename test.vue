71c7915b0dfeba18cac0a7804708067ca6db6761
zzx <zhengzhenxin@mingy-iot.com>
2023-05-06 15:43:17 +0800

    测试推送

---
+++ test.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class=""></div>
+</template>
+
+<script>
+export default {
+  name: "",
+  components: {},
+  data() {
+    return {};
+  },
+  //监听属性
+  computed: {},
+  //监控data中的数据变化
+  watch: {},
+  created() {},
+  mounted() {},
+  methods: {},
+};
+</script>
+<style lang="scss" scoped></style>
